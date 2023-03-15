dccbfbf036f5bb8ba1a3fd9df1f04b970398097a
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-03-15 12:58:10 -0500

    completed labwork

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+    return(
+        <>
+            <button
+            onBlur={()=> console.log('Hey! Eyes on me!')} 
+            onFocus={()=> console.log('Good!')}>
+                Eyes on me
+            </button>
+        </>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
 
+import React from "react";
+
 function Keypad (){
     return (
-        <div></div>
+        <>
+            <input type='password' onChange={() => console.log('Entering password...')}>
+
+            </input>
+        </>
     )
 }
 
